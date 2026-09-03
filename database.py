d24d28446c7c98f906c51f76cdc00401154f1a87
Ashwath S <achusatu@gmail.com>
2026-09-03 22:41:35 +0530

    Prepare KAPA Bank for Vercel deployment

--- database.py
+++ database.py
@@ -9,7 +9,10 @@ def get_connection():
     connection = oracledb.connect(
         user=os.getenv("ORACLE_USER"),
         password=os.getenv("ORACLE_PASSWORD"),
-        dsn=os.getenv("ORACLE_DSN")
+        dsn=os.getenv("ORACLE_DSN"),
+        config_dir=os.getenv("ORACLE_WALLET_PATH"),
+        wallet_location=os.getenv("ORACLE_WALLET_PATH"),
+        wallet_password=os.getenv("ORACLE_WALLET_PASSWORD")
     )
 
     return connection
